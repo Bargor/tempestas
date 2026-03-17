91db95ff82315523561be0807b02504fcf3e31d6
Karol Kontny <barolek@gmail.com>
2026-03-17 09:58:14 +0100

    Add readme and fix the wayland build

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <fmt/printf.h>
 #include <GLFW/glfw3.h>
 
 bool initialize_glfw() {
@@ -16,6 +17,8 @@ int main(int argc, char* argv[]) {
     if (!initialize_glfw()) {
         return EXIT_FAILURE;
     }
+
+    fmt::printf("GLFW initialized successfully\n");
 
     shutdown_glfw();
     return EXIT_SUCCESS;
