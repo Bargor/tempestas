4a18f3737da6c41f0b918320bf666ee8140af3f4
Karol Kontny <barolek@gmail.com>
2026-05-27 00:56:35 +0200

    Add event class

---
+++ src/application/event.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <device/keyboard.h>
+#include <device/mouse.h>
+#include <application/window.h>
+
+#include <chrono>
+#include <cstdint>
+#include <variant>
+
+namespace tst::application {
+
+struct event {
+    struct mouse_position {
+        double x{};
+        double y{};
+    };
+
+    struct mouse_button {
+        device::mouse_button button{};
+        device::mouse_action action{};
+        int32_t mods{};
+    };
+
+    struct scroll {
+        double x_offset{};
+        double y_offset{};
+    };
+
+    struct keyboard {
+        device::keys key{};
+        int32_t scancode{};
+        device::key_action action{};
+        int32_t mods{};
+    };
+
+    struct iconify {
+        window::window_display_state state{};
+    };
+
+    struct focus {
+        window::focus_mode mode{};
+    };
+
+    struct framebuffer_size {
+        core::extent<int32_t> size{};
+    };
+
+    struct closed {};
+
+    struct visibility {
+        window::visibility_mode mode{};
+    };
+
+    struct cursor_mode {
+        window::cursor_mode mode{};
+    };
+
+    struct time {
+        std::chrono::microseconds value{};
+    };
+
+    using payload = std::variant<mouse_position,
+                                 mouse_button,
+                                 scroll,
+                                 keyboard,
+                                 iconify,
+                                 focus,
+                                 framebuffer_size,
+                                 closed,
+                                 visibility,
+                                 cursor_mode,
+                                 time>;
+
+    const void* source{};
+    payload data{};
+};
+
+} // namespace tst::application
